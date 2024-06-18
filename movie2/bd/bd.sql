286c877a3895bec8e80b8018df9c3ff9c36e6c7c
tiagoalcantara06 <tiagoalcantara.ti06@gmail.com>
2024-06-17 21:05:48 -0300

    sim

--- movie2/bd/bd.sql
+++ movie2/bd/bd.sql
@@ -6,40 +6,20 @@ CREATE DATABASE movie;
 
 USE movie;
 
-CREATE TABLE RA (
-    cod_ra int AUTO_INCREMENT PRIMARY KEY,
-    RA VARCHAR(120),
-    nome_funcionario VARCHAR(120)
+CREATE TABLE login (
+id int AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(120),
+email VARCHAR(120),
+senha VARCHAR(120)
 );
 
 CREATE TABLE login_adm (
-cod_adm int AUTO_INCREMENT PRIMARY KEY,
-senha_adm Varchar(120),
-isOf boolean DEFAULT true,
-cod_ra int,
-FOREIGN KEY (cod_ra) REFERENCES RA(cod_ra)
-);
-
-CREATE TABLE usuario (
-    id int AUTO_INCREMENT PRIMARY KEY,
-    nome Varchar(120),
-    email Varchar(120),
-    senha Varchar(120),
-    isOf boolean DEFAULT false 
+id int AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(120),
+email VARCHAR(120),
+senha_adm VARCHAR(120)
 );
 
 
-INSERT INTO RA (RA, nome_funcionario) VALUES 
-('RA123', 'João Silva'),
-('RA456', 'Maria Oliveira'),
-('RA789', 'Pedro Souza'),
-('RA101', 'Ana Santos'),
-('RA202', 'Carlos Pereira');
 
-INSERT INTO login_adm (cod_ra, senha_adm) VALUES 
-(1, 'senha'),
-(2, 'senha'),
-(3, 'senha'),
-(4, 'senha'),
-(5, 'senha');
 
